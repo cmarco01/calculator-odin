d7f15d39f88c03828ab2571b053951815e760e10
Chris Marcoullier <157416942+cmarco01@user.noreply.github.com>
2024-03-15 19:03:58 -0400

    Update, finished so it calculates op w/ correct order of operations

--- javascript.js
+++ javascript.js
@@ -55,132 +55,11 @@ function createKeys() {
 createKeys();
 
 
-
-function testNum(arr) {
-    let nums = [];
-    let num = "";
-    let operator = "";
-    let operatorChosen = false;
-    let validOperators = ["+", "-", "/", "*"];
-
-    // iterate thru
-    // if i is a num, add it to string
-    // if i is NOT a num (aka an operator):
-    //      add that parseFloat(string) to nums
-    //      clear it
-    // set i to operator
-
-    let tempArr = [];
-    let numDigits = 0;
-
-    for (let i = 0; i < arr.length + 1; i++) {
-        // console.log(arr[i]);
-        // console.log(typeof(arr[i]));
-        let digit = arr[i];
-        
-        
-        if (typeof(digit) != "number") {
-            
-            nums.push(num);
-            // console.log("ADDING " + num + " to list");
-            num = "";
-            if (validOperators.includes(digit) && operatorChosen == false) {
-                operator = digit;
-                operatorChosen = true;
-            } else if (validOperators.includes(digit) && operatorChosen == true) {
-                // console.log("FOUND SECOND OPERATOR! breaking for loop");
-                break;
-            }
-        } else {
-            num += [String(arr[i])];
-        }
-
-        // using this to get the starting index to slice out the remainder of the op
-        numDigits += 1;
-
-        // console.log('num: ' + num);
-    }
-
-    if (nums.includes('')) {
-        nums.pop();
-    }
-
-    console.log(nums);
-    // console.log("operator: " + operator);
-
-    let firstNum = parseFloat(nums[0]);
-    let secondNum = parseFloat(nums[1]);
-    let answer = 0;
-
-    if (operator == "+") {
-        answer = firstNum + secondNum;
-    } else if (operator == "-") {
-        answer = firstNum - secondNum;
-    } else if (operator == "*") {
-        answer = firstNum * secondNum;
-    } else if (operator == "/") {
-        answer = firstNum / secondNum;
-    }
-
-    // console.log("ANSWER:  " + answer);
-    
-    remainingOp = arr.slice(numDigits);
-    remainingOp.unshift(answer);
-
-    // console.log("REMAINING OP: " + remainingOp);
-
-
-    let finalAnswer = false;
-
-    for (let i = 0; i < remainingOp.length; i++) {
-        finalAnswer = true;
-        if (validOperators.includes(remainingOp[i])) {
-            // console.log("OP NOT COMPLETE! Running again...");
-            finalAnswer = false;
-            testNum(remainingOp);          
-            break;
-        }
-    }
-    
-    if (finalAnswer == true) {
-        console.log("FINAL ANSWER " + answer);
-        return answer;
-    }
-    
-
-}
-
-let arr = [5, "*", 3, "*", 10, "-", 1, "/", 2];
-
-// testNum(arr);
+let arr = [5, "+", 3, "*", 1, 0, "-", 1, "+", 2];
 getOperationArray(arr);
-
-/*
-ok this is a DOOZY! haha, but it works. 
-
-all i need to do;
--track what key was pressed and add it to an array (like above)
-    -if it's a number, it needs to be a NUMBER! won't work if i add a number as a string, i.e. "5"
-    -operators need to be strings
--when = key is pressed, send the array to this function
--it will return the FINAL ANSWER
-
--*can also modify to send operation, or do that in separate function-- for the display
-
-*/
 
 
 function getOperationArray(arr) {
-    /*
-    iterate thru array
-    if digit is a number, concat to num
-    if digit is NOT a number:
-        -parseFloat num
-        -add num to opArr
-        -add operator to opArr
-        -clear num
-    */
-
     let opArr = [];
     let num = "";
     let operator = "";
@@ -203,114 +82,73 @@ function getOperationArray(arr) {
     }
 
     console.log("operation: " + opArr);
-
-
-    // console.log("Previous Operation (no PEMDAS): " + opArr);
-    // opArr = getOrderOfOperations(opArr);
-    // console.log("Operation w/ PEMDAS: " + opArr);
+    calcOperation(opArr);
 }
 
-function getOrderOfOperations(arr) {
-    // takes an operation as array
-    // returns it in correct order of operations
 
-    // by nature, an operation will ALWAYS have one more number than operator
-    // ex. 2 + 2 -- [2 nums, 1 op]   or   3 * 5 + 10 - 4 -- [4 nums, 3 ops]
+function calcOperation(arr) {
+    let num1 = 0;
+    let num2 = 0;
+    let operator = "";
+    let index = 0;
 
-    // if array is [5, "+", 3, "*", 10, "-", 1, "/", 2] (equals 19 w/ order of operations PEMDAS)
-    // we want [3, "*", 10, "/", 2, "+", 5, "-", 1]
+    let validOperators = ["+", "-", "/", "*"];
 
-    // len of array = 9
-    // operators will always be at ODD indexes (in this case) 1, 3, 5, (len - 2)
-
-    // first, get an array of operators 
-    let operators = [];
     for (let i = 0; i < arr.length; i++) {
-        if (i % 2 == 1) {
-            operators.push(arr[i]);
-        }
-    }
-
-    // console.log(operators);
-    let i = 1;
+        let digit = arr[i];
     
-    for (i; i < operators.length; i++) {
-        // console.log(i);
-        let thisOp = operators[i];
-        let prevOp = operators[i-1];
-        // console.log("thisOp: " + thisOp + "\nprevOp: " + prevOp);
-        if (thisOp == "*" || thisOp == "/") {
-            if (prevOp == "+" || prevOp == "-") {
-                operators[i-1] = thisOp;
-                operators[i] = prevOp;
-                i = 0;
+        if (arr.includes("*") || arr.includes("/")) {
+            if (typeof(digit) != "number") {
+                if (digit == "*" || digit == "/") {
+                    num1 = arr[i - 1];
+                    num2 = arr[i + 1];
+                    operator = digit;
+                    index = i - 1;
+                    break;
+                }
+            }
+
+        } else {
+            if (typeof(digit) != "number") {
+                num1 = arr[i-1];
+                num2 = arr[i+1];
+                operator = digit;
+                index = i - 1;
+                break;
             }
         }
 
-        // console.log(operators);
     }
 
-    console.log(operators);
+    arr.splice(index, 3);
+    ans = computeOp(num1, operator, num2);
+    arr.splice(index, 0, ans);
 
-    // okay, now we have ops in correct order
-    /*
-    first, find index operators[0] in arr
-    for first element in NEW OP ARR, add the arr at; index - 1, operators[0], index + 1
-    just for first element!!
+    // console.log(arr);
 
-    for the next, we need to just find the index of the next operator, and then add that and arr[index+1]
-    */
-    let firstOpIndex = arr.indexOf(operators[0]);
-    // console.log(firstOpIndex); // expecting 3
-
-    let firstNum = arr[firstOpIndex - 1];
-    let secondNum = arr[firstOpIndex + 1];
-    let newArr = [firstNum, operators[0], secondNum];
-    // console.log(newArr); // expecting 3, "*", 10
-
-
-    // filter this out of array, then reverse
-    let sliceOp = arr.slice(0, firstOpIndex-1).reverse();
-
-    for (let i = 0; i < sliceOp.length; i++) {
-        digitIndex = arr.indexOf(sliceOp[i]);
-        arr.splice(digitIndex, 1);
-        arr.push(sliceOp[i]);
+    for (let i = 0; i < arr.length; i++) {
+        if (validOperators.includes(arr[i])) {
+            calcOperation(arr);
+            break;
+        }
     }
 
-    console.log(arr);
-    console.log("newArr; " + newArr);
+    console.log("answer: " + ans);
 
-    let k = 3;
-    for  (k = 3; k < arr.length; k++) {
-        op = arr[k];
-        num = arr[k+1];
-        newArr.push(op);
-        newArr.push(num);
-        k += 1;
-    }
-    
-    console.log("newArr == > ; " + newArr);
-
-    // for (let i = 1; i < operators.length; i++) {
-    //     opIndex = arr.indexOf(operators[i]);
-    //     num = arr[opIndex + 1];
-    //     console.log(num );
-    //     newArr.push(operators[i]);
-    //     newArr.push(num);
-    // }
-
-    // console.log(newArr);
-
-    return newArr;
-
+    return ans;
 }
 
-/* 
+function computeOp(firstNum, operator, secondNum) {
+    if (operator == "+") {
+        answer = firstNum + secondNum;
+    } else if (operator == "-") {
+        answer = firstNum - secondNum;
+    } else if (operator == "*") {
+        answer = firstNum * secondNum;
+    } else if (operator == "/") {
+        answer = firstNum / secondNum;
+    }
 
-taking a break;
+    return answer;
+}
 
-good thing was that i got the full operation as it's typed so i can
-
-but even then do i need that? cuz i can display stuff w/o that
-*/
